20bf3c5eb2efd8e69d53a50a2dfea638abcef813
Norman Rzepka <code@normanrz.com>
2017-01-03 14:43:55 +0100

    fixed 20W benchmark

--- run_threads_benchmark.sh
+++ run_threads_benchmark.sh
@@ -5,7 +5,7 @@ set -e
 if [[ -e results/tpcc_single_run/\{\}/None/ab.log ]]; then
   rm results/tpcc_single_run/\{\}/None/ab.log
 fi
-for i in {20..20}
+for i in {1..32}
 do
   echo $i threads
   python exp_tpcc_single_run.py --ab=/home/Norman.Rzepka/tpcc/queries_gen/20W_1M.txt --clients=10 --threads=$i --abCore=22 --port=5222 --tabledir=/home/Norman.Rzepka/tpcc/20W-tables/ --duration=60 --verbose=1 --stdout --stderr
