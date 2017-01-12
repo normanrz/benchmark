2c05d71bf1cc5ba424babf7e0ceb92f9fb5ff2b7
Norman Rzepka <code@normanrz.com>
2017-01-12 16:40:56 +0100

    fixed run_threads_benchmark

--- run_threads_benchmark.sh
+++ run_threads_benchmark.sh
@@ -8,7 +8,7 @@ set -e
 if [[ -e results/tpcc_single_run/\{\}/None/ab.log ]]; then
   rm results/tpcc_single_run/\{\}/None/ab.log
 fi
-for i in {$LOWER_THREADS..$UPPER_THREADS}
+for i in $(seq ${LOWER_THREADS} ${UPPER_THREADS})
 do
   echo $i threads
   python exp_tpcc_single_run.py --ab=/home/Norman.Rzepka/tpcc/queries_gen/20W_1M.txt --clients=10 --threads=$i --abCore=22 --port=5222 --tabledir=/home/Norman.Rzepka/tpcc/20W-tables/ --duration=60 --verbose=1 --stdout --stderr
