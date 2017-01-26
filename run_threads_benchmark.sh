bff7c452821599ff1e41b2b9715579b25470509c
Norman Rzepka <code@normanrz.com>
2017-01-26 11:05:28 +0100

    new benchmark, increased client count to 1000

--- run_threads_benchmark.sh
+++ run_threads_benchmark.sh
@@ -11,7 +11,7 @@ fi
 for i in $(seq ${LOWER_THREADS} ${UPPER_THREADS})
 do
   echo $i threads
-  python exp_tpcc_single_run.py --ab=/home/Norman.Rzepka/tpcc/queries_gen/20W_1M.txt --clients=10 --threads=$i --abCore=22 --port=5222 --tabledir=/home/Norman.Rzepka/tpcc/20W-tables/ --duration=60 --verbose=1 --stdout --stderr
+  python exp_tpcc_single_run.py --ab=/home/Norman.Rzepka/tpcc/queries_gen/20W_1M.txt --clients=1000 --threads=$i --abCore=22 --port=5222 --tabledir=/home/Norman.Rzepka/tpcc/20W-tables/ --duration=60 --verbose=1 --stdout --stderr
   mv results/tpcc_single_run/\{\}/None/ab.log results/tpcc_single_run/\{\}/None/ab_threads_$(printf "%02d" $i).log
 #  rm results/tpcc_single_run/\{\}/None/ab.log
 done
